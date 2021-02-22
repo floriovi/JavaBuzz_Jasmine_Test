3547b363853207536d9ee42bab9e25160a8f1264
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-02-22 18:19:14 +0000

    Refactoring and adding says method and tests to play fizzbuzz

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -10,15 +10,15 @@ describe('Javabuzz', function() {
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      expect(javabuzz._isDivisibleBy(3, 3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     }); 
 
     it('divisible by 5', function() {
-      expect(javabuzz._isDivisibleBy(5, 5)).toBe(true);
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
 
     it('divisible by 15', function() {
-      expect(javabuzz._isDivisibleBy(15, 15)).toBe(true);
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
   });
@@ -26,17 +26,37 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('divisible by 3', function() {
-      expect(javabuzz._isDivisibleBy(1, 3)).toBe(false);
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
 
     it('divisible by 5', function() {
-      expect(javabuzz._isDivisibleBy(1, 5)).toBe(false);
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
 
     it('divisible by 15', function() {
-      expect(javabuzz._isDivisibleBy(1, 15)).toBe(false);
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
 
   });
 
+  describe('when playing, says', function(){
+    
+    it('"Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"JavaBuzz" when a number is divisible by 15', function() {
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
+
+    it('11 when a number is not divisible by either 3, 5 or 15 ', function(){
+      expect(javabuzz.says(11)).toEqual(11);
+    });
+
+  });
+  
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -1,7 +1,32 @@
 class Javabuzz {
 
+  // The underscore before the method tells us that it is public
+
   _isDivisibleBy(number, divisor) {
     return (number % divisor === 0);
-  };
+  }
 
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+  
+  isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return "JavaBuzz";
+    } else if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    } else if (this.isDivisibleByThree(number)) {
+      return "Java";
+    } else {
+      return number;
+    }
+  }
 }
